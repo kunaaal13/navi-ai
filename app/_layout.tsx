04369fd5bd0070a66c6108c44fae794fbf50e983
Kunaaal13 <mrkunalyadav7@gmail.com>
2024-03-15 17:15:06 +0530

    Added welcome screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,40 @@
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
 import '../global.css';
 
-import { Stack } from 'expo-router';
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from 'expo-router';
 
-export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
-};
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    Geist: require('../assets/fonts/GeistMono.ttf'),
+  });
+
+  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
   return (
     <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
     </Stack>
   );
 }
---
+++ app/index.tsx
@@ -0,0 +1,32 @@
+import LottieView from 'lottie-react-native';
+import { SafeAreaView, Text, TouchableOpacity, View } from 'react-native';
+
+function Welcome() {
+  return (
+    <SafeAreaView className="bg-black flex-1 items-center justify-between w-full">
+      <View className="flex-1 items-center justify-between w-4/5 py-10">
+        <View className="items-center justify-center gap-5 mt-10">
+          <Text className="text-white text-5xl font-bold">Navi AI</Text>
+          <Text className="text-white text-2xl font-semibold text-center">
+            Chat like a friend, learn like an expert.
+          </Text>
+        </View>
+
+        <LottieView
+          autoPlay
+          style={{
+            width: 250,
+            height: 250,
+          }}
+          source={require('../assets/robot.json')}
+        />
+
+        <TouchableOpacity className="bg-blue-700 p-4 rounded-xl items-center w-full">
+          <Text className="text-white text-2xl font-semibold">Get Started</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default Welcome;
